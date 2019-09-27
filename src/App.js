3a32ba2c6add578862c980a97a4e9f856a9bc03d
Asto7 <ssatyanand7@gmail.com>
2019-09-27 19:07:09 +0530

    hey

--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
-import React from 'react';
 import Navbar from './component/Navbar.js'
 import Content from './component/Content.js'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-function App() {
-    var data=[
-       {"name":"KIRA","age":11,"game":"FootBall","child":[
-                                        {"name":"Light","age":15,"game":"Cricket","child":[
-                                                                         {"name":"Asto","age":21,"game":"BaskEtBall","child":[]},
-                                                                         {"name":"XYZ","age":12,"game":"BaskEtBall","child":[
-                                                                         {"name":"A","age":4,"game":"BaskEtBall","child":[
-                                                                         {"name":":)","age":34,"game":"VolleyBall","child":[]},
+//Getting data From public folder
+export default class App extends Component {
 
-                                                                         ]},
-                                                                         {"name":"B","age":45,"game":"BaskEtBall","child":[]}
-
-                                                                         ]},
-                                                                         {"name":"C","age":14,"game":"BaskEtBall","child":[]}
-
-                                                                        ]}
-                                       ]},
-       {"name":"D","age":10,"game":"HandBall","child":[]},
-       {"name":"E","age":60,"game":"Hockey","child":[]},
-       {"name":"F","age":40,"game":"Guitar","child":[]},
-       {"name":"G","age":35,"game":"Drums","child":[]},
-
-     ]
-
-
-  return (
-    <div className="App ">
-      <Navbar></Navbar>
-      <div className="container">
-      <Content data={data}></Content>
-        </div>
-    </div>
-  );
+    state={data:[]}
+    componentDidMount() {
+      axios.get('/data.json')
+        .then(res => {
+            this.setState(res.data);
+            console.log(this.state.data)
+        })
+    }
+  render() {
+        return (
+                <div className="App ">
+                <Navbar></Navbar>
+                <div className="container">
+                { <Content data={this.state.data}></Content>}
+                    </div>
+                </div>
+            )
+    }
 }
-
-export default App;
